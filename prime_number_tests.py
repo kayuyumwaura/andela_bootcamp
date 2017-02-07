b12d27ff75c6ef91a69455ccf8946045a99657c1
LENNY\kayuyu <mwaurakayuyu@gmail.com>
2017-02-07 09:11:26 +0300

    new commit

--- prime_number_tests.py
+++ prime_number_tests.py
@@ -17,6 +17,10 @@ class TestPrime(unittest.TestCase):
 	def test_prime(self):
 		self.assertEqual(prime.prime(30), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
 
+# Test if function correctly returns prime numbers. 
+	def test_prime(self):
+		self.assertEqual(prime.prime(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+
 # Test if function returns there are o prime numbers if number less than 0 entered.
 	def test_less_than_zero(self):
 		self.assertEqual(prime.prime(0))
@@ -29,3 +33,4 @@ class TestPrime(unittest.TestCase):
 	# Test if function returns an error message if wrong data type passed to function.
 	def test_invalid_type(self):
 		self.assertEqual(prime.prime("String"))
+ 
