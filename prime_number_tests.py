20adb46b9d2efb5e48972b3e8eba566ad3f9ada0
LENNY\kayuyu <mwaurakayuyu@gmail.com>
2017-02-07 08:57:33 +0300

    new commit

--- prime_number_tests.py
+++ prime_number_tests.py
@@ -15,7 +15,7 @@ class TestPrime(unittest.TestCase):
 
 # Test if function correctly returns prime numbers. 
 	def test_prime(self):
-		self.assertEqual(prime.prime(20), [2, 3, 5, 7, 11, 13, 17, 19])
+		self.assertEqual(prime.prime(30), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
 
 # Test if function returns there are o prime numbers if number less than 0 entered.
 	def test_less_than_zero(self):
